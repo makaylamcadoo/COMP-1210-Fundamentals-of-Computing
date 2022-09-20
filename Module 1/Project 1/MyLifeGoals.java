2b507c0a7b1e7be28b89ca682f75470b42dfb011
Makayla McAdoo <111717594+makaylamcadoo@users.noreply.github.com>
2022-09-20 14:37:04 -0500

    Create MyLifeGoals.java

---
+++ Module 1/Project 1/MyLifeGoals.java
@@ -0,0 +1,32 @@
+/**
+*Simple program that prints my name and life goals.
+*
+*Project 1
+*@author Makayla McAdoo - COMP 1210
+*@version 08/25/2021
+*/
+public class MyLifeGoals
+{
+   /**
+   *Prints Name one time.
+   *Prints short,medium and long-term life goals one time.
+   *
+   *@param args Command line arguments - not used
+   */
+   public static void main(String[]args)
+   {
+      System.out.println("Name: Makayla McAdoo");
+      System.out.println();
+      System.out.println("My short-term life goal is to make it through my"
+         + " freshmen year of college and learn time management"
+         + " so I can make all A's and high B's.");
+      System.out.println("My medium-term life goal is to graduate college and"
+         + " be able to stand on my own two feet as an adult without"
+         + " needing too many handouts from my parents.");
+      System.out.println("My long-term life goal is to find and work in a"
+         + " career that fulfills me and my creativity,and"
+         + " allows me to take care of myself and the family I"
+         + " create with someone I love.");
+         
+   }
+}

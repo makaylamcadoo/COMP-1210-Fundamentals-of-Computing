1648203025c9b266fcc25f1b3103402b8da244e1
Makayla McAdoo <111717594+makaylamcadoo@users.noreply.github.com>
2022-09-20 14:38:57 -0500

    Create StudentInfo.java

---
+++ Module 1/Activity 1/StudentInfo.java
@@ -0,0 +1,23 @@
+   /**
+   *Simple program to write three or more print statements.
+   *
+   Activity 1
+   @author Makayla McAdoo- COMP1210
+   @version 08/23/2021
+   */
+public class StudentInfo
+{
+   /**
+   *Prints Name one time.
+   *Prints Computer Courses one time
+   *
+   *@param args Command line arguments - not used
+
+   */
+   public static void main(String[]  args)
+   {
+      System.out.println("Name: Makayla McAdoo");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   None");
+   }
+}

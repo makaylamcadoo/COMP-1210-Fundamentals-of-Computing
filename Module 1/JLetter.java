cb0cbfb1b85b397631b04fa6aeed489555c4a3fe
Makayla McAdoo <111717594+makaylamcadoo@users.noreply.github.com>
2022-09-19 22:52:42 -0500

    Create JLetter.java

---
+++ Module 1/JLetter.java
@@ -0,0 +1,28 @@
+/**
+*Simple program that prints the letter J usig J,A,V,A
+*
+*Project 1
+*@author Makayla McAdoo - COMP 1210
+*@version 08/25/2021
+*/
+public class JLetter
+{
+   /**
+   *Prints J as a block letter using the characters of Java.
+   *
+   *@param args Command line arguments - not used
+   */
+   public static void main(String[]args)
+   {
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("JAVAJAVAJAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("      JAVA");
+      System.out.println("J     JAVA");
+      System.out.println("JA    JAVA");
+      System.out.println(" JAVAJAVA ");
+      System.out.println("  JAVAJA  ");
+   }
+}
